6ff9f5bb91633a47386cb0a611137e1d6ef8aeb1
Mogeni <evansmogeni254@gmail.com>
2024-10-09 04:10:48 +0300

    add gitgnore file

--- src/app.ts
+++ src/app.ts
@@ -39,63 +39,71 @@ const todoSchema = new mongoose.Schema({
 });
 
 // Define the model
-const TodoModel = mongoose.model("tasks", todoSchema);
+const TaskModel = mongoose.model("tasks", todoSchema);
 
 
 
-let todos: Todo[] = [
-  { id: "1", title: "Learn TypeScript", completed: false },
-  { id: "2", title: "Learn Node.js", completed: false },
-  { id: "3", title: "Learn Express", completed: false },
-  { id: "7", title: "Learn MongoDB", completed: false },
-  { id: "11", title: "Learn Redux", completed: false },
-];
+// let todos: Todo[] = [
+//   { id: "1", title: "Learn TypeScript", completed: false },
+//   { id: "2", title: "Learn Node.js", completed: false },
+//   { id: "3", title: "Learn Express", completed: false },
+//   { id: "7", title: "Learn MongoDB", completed: false },
+//   { id: "11", title: "Learn Redux", completed: false },
+// ];
 
 // Get all todos
-app.get("/todos", (req: any, res: any) => {
-  res.json(todos);
+// app.get("/todos", (req: any, res: any) => {
+//   res.json(todos);
+// });
+
+// Get all todos
+app.get("/getTasks", async (req: any, res: any) => {
+  const tasks = await TaskModel.find();
+  res.json(tasks);
 });
 
-// Create new todo
-app.post("/todos", (req: any, res: any) => {
-  const todo = {
-    id: uuidv4(),
-    title: req.body.title,
-    completed: req.body.completed || false,
-  };
-  todos.push(todo);
-  res.status(201).json(todo);
-});
 
-// Get todo by ID
-app.get("/todos/:id", (req: any, res: any) => {
-  const id = req.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  if (!todo) {
-    return res.status(404).json({ message: "Todo not found" });
-  }
-  res.json(todo);
-});
 
-// Update todo by ID
-app.patch("/todos/:id", (req: any, res: any) => {
-  const id = req.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  if (!todo) {
-    return res.status(404).json({ message: "Todo not found" });
-  }
-  todo.title = req.body.title || todo.title;
-  todo.completed = req.body.completed || todo.completed;
-  res.json(todo);
-});
+// // Create new todo
+// app.post("/todos", (req: any, res: any) => {
+//   const todo = {
+//     id: uuidv4(),
+//     title: req.body.title,
+//     completed: req.body.completed || false,
+//   };
+//   todos.push(todo);
+//   res.status(201).json(todo);
+// });
 
-// Delete todo by ID
-app.delete("/todos/:id", (req: any, res: any) => {
-  const id = req.params.id;
-  const index = todos.findIndex((todo) => todo.id === id);
-  if (index === -1) {
-    return res.status(404).json({ message: "Todo not found" });
-  }
-  todos.splice(index, 1);
-  res.status(204).json();
-});
+// // Get todo by ID
+// app.get("/todos/:id", (req: any, res: any) => {
+//   const id = req.params.id;
+//   const todo = todos.find((todo) => todo.id === id);
+//   if (!todo) {
+//     return res.status(404).json({ message: "Todo not found" });
+//   }
+//   res.json(todo);
+// });
+
+// // Update todo by ID
+// app.patch("/todos/:id", (req: any, res: any) => {
+//   const id = req.params.id;
+//   const todo = todos.find((todo) => todo.id === id);
+//   if (!todo) {
+//     return res.status(404).json({ message: "Todo not found" });
+//   }
+//   todo.title = req.body.title || todo.title;
+//   todo.completed = req.body.completed || todo.completed;
+//   res.json(todo);
+// });
+
+// // Delete todo by ID
+// app.delete("/todos/:id", (req: any, res: any) => {
+//   const id = req.params.id;
+//   const index = todos.findIndex((todo) => todo.id === id);
+//   if (index === -1) {
+//     return res.status(404).json({ message: "Todo not found" });
+//   }
+//   todos.splice(index, 1);
+//   res.status(204).json();
+// });
